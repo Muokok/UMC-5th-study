c70242d49473184235d7fa92ad01da86a723296f
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2024-02-26 17:45:37 +0900

    [Feat]: Health Check API 추가

---
+++ src/main/java/umc/spring/web/controller/HealthCheck.java
@@ -0,0 +1,15 @@
+package umc.spring.web.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "OK, GO GO";
+    }
+
+}
